8b8d989ec921fabc8e0cc3d117d6b842a81f0c4c
Reicela <reicelapetersone@gmail.com>
2022-09-23 14:29:29 +0300

    GetFlight, PutFlight

---
+++ FlightPlanner/FlightPlanner/Controllers/AdminApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("admin-api")]
+    [ApiController, Authorize]
+    public class AdminApiController : ControllerBase
+    {
+        [Route("flights/{id}")]
+        [HttpGet]
+        public IActionResult GetFlight(int id)
+        {
+            return NotFound();
+        }
+
+        [Route("flights")]
+        [HttpPut]
+        public IActionResult PutFlight(Flight flight)
+        {
+            return Created("", flight);
+        }
+    }
+}
