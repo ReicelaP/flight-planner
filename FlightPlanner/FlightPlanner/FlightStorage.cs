294371a51f15401897237fe2c44e138fdf51751a
Reicela <reicelapetersone@gmail.com>
2022-09-23 23:48:46 +0300

    Added Flight validation

--- FlightPlanner/FlightPlanner/FlightStorage.cs
+++ FlightPlanner/FlightPlanner/FlightStorage.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace FlightPlanner
 {
-    public class FlightStorage
+    public static class FlightStorage
     {
         private static List<Flight> _flights = new List<Flight>();
         private static int _id = 0;
@@ -24,6 +24,44 @@ namespace FlightPlanner
         {
             _flights.Clear();
             _id = 0;
+        }
+
+        public static bool IsUniqueFlight(Flight flight)
+        {
+            foreach (var f in _flights)
+            {
+                if(flight.From.Country == f.From.Country &&
+                    flight.From.City == f.From.City &&
+                    flight.From.AirportCode == f.From.AirportCode &&
+                    flight.To.Country == f.To.Country &&
+                    flight.To.City == f.To.City &&
+                    flight.To.AirportCode == f.To.AirportCode &&
+                    flight.Carrier == f.Carrier &&
+                    flight.DepartureTime == f.DepartureTime &&
+                    flight.ArrivalTime == f.ArrivalTime)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsValidValue(Flight flight)
+        {
+            return 
+                flight != null &&
+                flight.From != null &&
+                flight.To != null &&
+                !string.IsNullOrEmpty(flight.From.Country) &&
+                !string.IsNullOrEmpty(flight.From.City) &&
+                !string.IsNullOrEmpty(flight.From.AirportCode) &&
+                !string.IsNullOrEmpty(flight.To.Country) &&
+                !string.IsNullOrEmpty(flight.To.City) &&
+                !string.IsNullOrEmpty(flight.To.AirportCode) &&
+                !string.IsNullOrEmpty(flight.Carrier) &&
+                !string.IsNullOrEmpty(flight.DepartureTime) &&
+                !string.IsNullOrEmpty(flight.ArrivalTime);
         }
     }
 }
