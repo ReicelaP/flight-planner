21c33be7bc59a604de02839fe3e682693f0eb88d
Reicela <reicelapetersone@gmail.com>
2022-09-23 14:58:56 +0300

    Added class FlightStorage

---
+++ FlightPlanner/FlightPlanner/FlightStorage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace FlightPlanner
+{
+    public class FlightStorage
+    {
+        private static List<Flight> _flights = new List<Flight>();
+        private static int _id = 0;
+
+        public static Flight AddFlight(Flight flight)
+        {
+            flight.Id = ++_id;
+            _flights.Add(flight);
+            return flight;
+        }
+
+        public static void Clear()
+        {
+            _flights.Clear();
+            _id = 0;
+        }
+    }
+}
