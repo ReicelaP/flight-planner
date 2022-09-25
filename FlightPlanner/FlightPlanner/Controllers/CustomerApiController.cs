a351cde2549423bd93a9c4c970f6727e1ece07ff
Reicela <reicelapetersone@gmail.com>
2022-09-25 17:01:40 +0300

    Added GetAirport method

---
+++ FlightPlanner/FlightPlanner/Controllers/CustomerApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CustomerApiController : ControllerBase
+    {
+        [Route("airports")]
+        [HttpGet]
+        public IActionResult GetAirport(string search)
+        {
+            var result = FlightStorage.SearchAirport(search);
+            return Ok(result);
+        }
+    }
+}
