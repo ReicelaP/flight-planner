1e171f466f774b0bf30b6beac31446278facf94a
Reicela <reicelapetersone@gmail.com>
2022-09-25 20:46:49 +0300

    Added FindFlights method

--- FlightPlanner/FlightPlanner/Controllers/CustomerApiController.cs
+++ FlightPlanner/FlightPlanner/Controllers/CustomerApiController.cs
@@ -13,5 +13,32 @@ namespace FlightPlanner.Controllers
             var result = FlightStorage.SearchAirport(search);
             return Ok(result);
         }
+
+        [Route("flights/{id}")]
+        [HttpGet]
+        public IActionResult FindFlightById(int id)
+        {
+            if (FlightStorage.IsExistingFlight(id))
+            {
+                var result = FlightStorage.GetFlight(id);
+                return Ok(result);
+            }
+            
+            return NotFound();
+            
+        }
+
+        [Route("flights/search")]
+        [HttpPost]
+        public IActionResult FindFlights(SearchFlightsRequest request)              
+        {
+            if (SearchFlightsRequest.IsValidSearch(request) && 
+                SearchFlightsRequest.IsValidDestinationAirport(request))
+            {
+                return Ok(FlightStorage.GetFlightsInfoFromSearch(request));
+            }
+
+            return BadRequest();         
+        }
     }
 }
