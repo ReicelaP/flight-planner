536cf65456ac0b1a3f28f7109dac3c59eec6100d
Reicela <reicelapetersone@gmail.com>
2022-09-25 17:00:01 +0300

    Formating changes

--- FlightPlanner/FlightPlanner/Airport.cs
+++ FlightPlanner/FlightPlanner/Airport.cs
@@ -4,10 +4,10 @@ namespace FlightPlanner
 {
     public class Airport
     {
-        public string Country { get; set; }
-        public string City { get; set; }
-
         [JsonPropertyName("airport")]
         public string AirportCode { get; set; }
+
+        public string City { get; set; }
+        public string Country { get; set; }
     }
 }
