8f9debe028d631762ab2c42db0ab7a2cf8511f95
Reicela <reicelapetersone@gmail.com>
2022-09-26 17:47:12 +0300

    Formating changes

--- FlightPlanner/FlightPlanner/SearchFlightsRequest.cs
+++ FlightPlanner/FlightPlanner/SearchFlightsRequest.cs
@@ -5,24 +5,6 @@
         public string From { get; set; }
         public string To { get; set; }
         public string DepartureDate { get; set; }
-
-        public static bool IsValidSearch(SearchFlightsRequest request)
-        {
-            return
-                !string.IsNullOrEmpty(request.From) ||
-                !string.IsNullOrEmpty(request.To) ||
-                !string.IsNullOrEmpty(request.DepartureDate);
-        }
-
-        public static bool IsValidDestinationAirport(SearchFlightsRequest request)
-        {
-            if (request.From.ToLower() == request.To.ToLower())
-            {
-                return false;
-            }
-
-            return true;
-        }
     }
 }
 
