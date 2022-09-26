45f9559f9c31e7fa5a7818925d9629877b83606e
Reicela <reicelapetersone@gmail.com>
2022-09-27 00:47:46 +0300

    Updated IsValidArrivalTime method

--- FlightPlanner/FlightPlanner/Validations/FlightStorageValidators.cs
+++ FlightPlanner/FlightPlanner/Validations/FlightStorageValidators.cs
@@ -23,9 +23,7 @@ namespace FlightPlanner.Validations
 
         public static bool IsValidDestinationAirport(Flight flight)
         {
-            if (flight.From.Country.ToLower() == flight.To.Country.ToLower() ||
-               flight.From.City.ToLower() == flight.To.City.ToLower() ||
-               flight.From.AirportCode.ToLower() == flight.To.AirportCode.ToLower())
+            if (flight.From.AirportCode.ToLower().Trim() == flight.To.AirportCode.ToLower().Trim())
             {
                 return false;
             }
@@ -35,12 +33,8 @@ namespace FlightPlanner.Validations
 
         public static bool IsValidArrivalTime(Flight flight)
         {
-            string[] start = flight.DepartureTime.Replace('-', ' ').Replace(':', ' ').Split(" ");
-            DateTime departure = new DateTime(int.Parse(start[0]), int.Parse(start[1]), int.Parse(start[2]), int.Parse(start[3]), int.Parse(start[4]), 0);
-
-            string[] end = flight.ArrivalTime.Replace('-', ' ').Replace(':', ' ').Split(" ");
-            DateTime arrival = new DateTime(int.Parse(end[0]), int.Parse(end[1]), int.Parse(end[2]), int.Parse(end[3]), int.Parse(end[4]), 0);
-
+            DateTime departure = DateTime.Parse(flight.DepartureTime);
+            DateTime arrival = DateTime.Parse(flight.ArrivalTime);
             int result = DateTime.Compare(arrival, departure);
 
             return result <= 0 ? false : true;
